d0c563b5f1f96321aee33045bcb6dfe83fcf2d21
deletloss <40878273+deletloss@users.noreply.github.com>
2018-11-14 20:22:26 +0800

    Add files via upload

---
+++ TestCase1.rb
@@ -0,0 +1,15 @@
+require 'selenium-webdriver'
+driver = Selenium::WebDriver.for:chrome
+#user will directed to main page
+driver.get "http://automationpractice.com"
+#user access login page
+driver.find_element(:link_text,'Sign in').click;
+#user enter credential to login
+driver.find_element(:id, "email").clear
+driver.find_element(:id, "email").send_keys "ws12345@mail.com"
+driver.find_element(:id, "passwd").click
+driver.find_element(:id, "passwd").clear
+driver.find_element(:id, "passwd").send_keys "jajal23"
+driver.find_element(:id,"SubmitLogin").click;
+#if this text appear then the test is passed
+puts "Test passed: Page validated"
