1ee384996c7e2360e12aeaaeaea50d4d9c9e32df
yvdhi <40878273+deletloss@users.noreply.github.com>
2018-11-16 18:43:04 +0800

    Add files via upload

---
+++ TestCase18.rb
@@ -0,0 +1,17 @@
+require 'selenium-webdriver'
+driver = Selenium::WebDriver.for:chrome
+
+#user will directed to main page
+driver.get "http://automationpractice.com"
+
+#user access TOU page
+driver.find_element(:link, "About us").click;
+
+sleep 3
+#if this text appear then the test is passed
+if
+driver.find_element(:class => "navigation_page").text.include?("About us")
+puts "Test passed: Page validated"
+else
+puts "Test failed"
+end
